f0aa1057d257de8cab59b35f5fa19e88e4da309a
SpatLyu <lyu.geosocial@gmail.com>
2024-06-23 01:26:24 +0800

    small modify

--- R/plot.R
+++ R/plot.R
@@ -20,32 +20,58 @@ plot.factor_detector = \(x, slicenum = 2, alpha = 0.05, ...) {
                                                 'Significant',
                                                 'Not Significant',
                                                 NA))
-  fig_factor = ggplot2::ggplot(g,
-                               ggplot2::aes(x = stats::reorder(variable,qv), y = qv)) +
-    ggplot2::geom_bar(stat = "identity",fill = "#bebebe",
-                      ggplot2::aes(alpha = significance)) +
-    ggplot2::geom_bar(data = dplyr::slice(g,1),
-                      ggplot2::aes(alpha = significance),
-                      stat = "identity",fill = "#ff0000") +
-    ggplot2::geom_text(data = dplyr::slice(g, seq(1,slicenum)),
-                       ggplot2::aes(label = round(qv,4)),
-                       hjust = 1.25, color = "black") +
-    ggplot2::geom_text(data = dplyr::slice(g, -seq(1,slicenum)),
-                       ggplot2::aes(label = round(qv,4)),
-                       hjust = -0.1, color = "black") +
-    ggplot2::scale_y_continuous(limits = c(0,round(max(g$qv) + 0.04,1)),
-                                breaks = seq(0,round(max(g$qv) + 0.04,1),by = 0.1),
-                                expand = c(0,0)) +
-    ggplot2::scale_alpha_manual(values = c("Not Significant" = 0.25),
-                                na.value = 0.85,name = '') +
-    ggplot2::coord_flip() +
-    ggplot2::theme_minimal() +
-    ggplot2::labs(x = "", y = "Q statistic") +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 0,hjust = 1,color = 'black'),
-                   axis.text.y = ggplot2::element_text(color = 'black'),
-                   legend.position = "inside",
-                   legend.justification.inside = c('right','bottom'),
-                   ...)
+  if (any(is.na(g$significance))) {
+    fig_factor = ggplot2::ggplot(g,
+                                 ggplot2::aes(x = stats::reorder(variable,qv), y = qv)) +
+      ggplot2::geom_bar(stat = "identity",fill = "#bebebe",
+                        ggplot2::aes(alpha = significance)) +
+      ggplot2::geom_bar(data = dplyr::slice(g,1),
+                        ggplot2::aes(alpha = significance),
+                        stat = "identity",fill = "#ff0000") +
+      ggplot2::geom_text(data = dplyr::slice(g, seq(1,slicenum)),
+                         ggplot2::aes(label = round(qv,4)),
+                         hjust = 1.25, color = "black") +
+      ggplot2::geom_text(data = dplyr::slice(g, -seq(1,slicenum)),
+                         ggplot2::aes(label = round(qv,4)),
+                         hjust = -0.1, color = "black") +
+      ggplot2::scale_y_continuous(limits = c(0,round(max(g$qv) + 0.04,1)),
+                                  breaks = seq(0,round(max(g$qv) + 0.04,1),by = 0.1),
+                                  expand = c(0,0)) +
+      ggplot2::scale_alpha_manual(values = c("Not Significant" = 0.25),
+                                  na.value = 0.85,name = '') +
+      ggplot2::coord_flip() +
+      ggplot2::theme_minimal() +
+      ggplot2::labs(x = "", y = "Q statistic") +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 0,hjust = 1,color = 'black'),
+                     axis.text.y = ggplot2::element_text(color = 'black'),
+                     legend.position = "inside",
+                     legend.justification.inside = c('right','bottom'),
+                     ...)
+  } else {
+    fig_factor = ggplot2::ggplot(g,
+                                 ggplot2::aes(x = stats::reorder(variable,qv), y = qv)) +
+      ggplot2::geom_bar(stat = "identity",fill = "#bebebe") +
+      ggplot2::geom_bar(data = dplyr::slice(g,1),
+                        stat = "identity",fill = "#ff0000") +
+      ggplot2::geom_text(data = dplyr::slice(g, seq(1,slicenum)),
+                         ggplot2::aes(label = round(qv,4)),
+                         hjust = 1.25, color = "black") +
+      ggplot2::geom_text(data = dplyr::slice(g, -seq(1,slicenum)),
+                         ggplot2::aes(label = round(qv,4)),
+                         hjust = -0.1, color = "black") +
+      ggplot2::scale_y_continuous(limits = c(0,round(max(g$qv) + 0.04,1)),
+                                  breaks = seq(0,round(max(g$qv) + 0.04,1),by = 0.1),
+                                  expand = c(0,0)) +
+      ggplot2::coord_flip() +
+      ggplot2::theme_minimal() +
+      ggplot2::labs(x = "", y = "Q statistic") +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 0,hjust = 1,color = 'black'),
+                     axis.text.y = ggplot2::element_text(color = 'black'),
+                     legend.position = "inside",
+                     legend.justification.inside = c('right','bottom'),
+                     ...)
+  }
+
   return(fig_factor)
 }
 
