3ac247624f39036f84368ae94115d0f55488698b
SpatLyu <lyu.geosocial@gmail.com>
2024-06-22 14:31:45 +0800

    increase description

--- R/spd_lesh.R
+++ R/spd_lesh.R
@@ -1,10 +1,21 @@
 #' @title SHAP power of determinants (SPD)
+#' @author Wenbo Lv \email{lyu.geosocial@gmail.com}
+#' @description
+#' Function for calculate SHAP power of determinants \eqn{SPD}.
+#' @details
+#' The power of SHAP power of determinants formula is
+#' \eqn{\theta_{x_j} \left( S \right) = \sum\limits_{s \in M \setminus \{x_j\}} \frac{|S|! \left(|M| - |S| - 1\right)!}{|M|!}\left(v \left(S \cup \left\{x_j\right\} \right) - v\left(S\right)\right)}
+#'
+#' @references
+#' Li, Y., Luo, P., Song, Y., Zhang, L., Qu, Y., & Hou, Z. (2023). A locally explained heterogeneity model for
+#' examining wetland disparity. International Journal of Digital Earth, 16(2), 4533–4552.
+#' https://doi.org/10.1080/17538947.2023.2271883
 #'
 #' @param formula A formula of calculate SHAP power of determinants \eqn{SPD}.
 #' @param data A data.frame or tibble of observation data.
 #' @param cores (optional) A positive integer(default is 1). If cores > 1, a 'parallel' package
 #' cluster with that many cores is created and used. You can also supply a cluster object.
-#' @param ... (optional) Other arguments passed to `gozh()`.
+#' @param ... (optional) Other arguments passed to `rpart_disc()`.
 #'
 #' @return A tibble with variable and its corresponding \eqn{SPD} value.
 #' @export
@@ -39,8 +50,8 @@ spd_lesh = \(formula,data,cores = 1,...){
 
   calcul_theta = \(xvar,fullxvar){
     n = length(fullxvar)
-    df1 = rpart_disc(paste0(yname,'~',paste(c(xvar,fullxvar),collapse = '+')),data = dti)
-    df2 = rpart_disc(paste0(yname,'~',paste(fullxvar,collapse = '+')),data = dti)
+    df1 = rpart_disc(paste0(yname,'~',paste(c(xvar,fullxvar),collapse = '+')),data = dti,...)
+    df2 = rpart_disc(paste0(yname,'~',paste(fullxvar,collapse = '+')),data = dti,...)
     v1 = factor_detector(dti[,yname,drop = TRUE],df1)[[1]]
     v2 = factor_detector(dti[,yname,drop = TRUE],df2)[[1]]
     thetax = factorial(n) * factorial(m - n - 1) * (v1 - v2) / mf
