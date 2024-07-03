4a394a90748d55dd669e2002d1d86f129d38f846
SpatLyu <lyu.geosocial@gmail.com>
2024-07-03 15:58:40 +0800

    small modify

--- R/r_interact_py.R
+++ R/r_interact_py.R
@@ -2,17 +2,12 @@
 #' @noRd
 #'
 utils_source_python = \(oauth_func_path) {
+  gdverse_pyinit()
   module_name = gsub("\\.py$", "", basename(oauth_func_path))
   module_path = dirname(oauth_func_path)
   reticulate::import_from_path(module_name,
                                path = module_path,
-                               convert = TRUE,
-                               delay_load = list(
-                                 before_load = gdverse_pyinit,
-                                 on_error = \(error) {
-                                   stop('python dependencies for gdverse cannot be loaded properly.')
-                                   }
-                               ))
+                               convert = TRUE)
 }
 
 #' Check Python & R sync
@@ -23,8 +18,13 @@ gdverse_pyinit = function() {
   gdverse_python = Sys.getenv("GDVERSE_PYTHON", unset = NA)
   if (!is.na(gdverse_python)) {
     reticulate::use_python(gdverse_python)
+  } else if (!all(purrr::map_lgl(
+    c('numpy','pandas','ruptures','joblib'),
+    reticulate::py_module_available))) {
+    stop('python dependencies for gdverse cannot be loaded properly.')
   } else {
     message("\n", "Please set `GDVERSE_PYTHON` environment variable to appropriate python path!")
-    message("\n", "Run `vignette('RGDRID',package = 'gdverse')` to check out vignettes in the RGDRID section for more details")
+    message("\n", "Run `vignette('RGDRID',package = 'gdverse')` to see more details")
+    stop('python dependencies for gdverse cannot be loaded properly.')
   }
 }
