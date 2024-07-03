cc7231ad41754daef1fb51f2aa46b5d2ab717bc6
SpatLyu <lyu.geosocial@gmail.com>
2024-07-03 15:42:05 +0800

    small modify

--- R/r_interact_py.R
+++ R/r_interact_py.R
@@ -4,7 +4,15 @@
 utils_source_python = \(oauth_func_path) {
   module_name = gsub("\\.py$", "", basename(oauth_func_path))
   module_path = dirname(oauth_func_path)
-  reticulate::import_from_path(module_name, path = module_path, convert = TRUE)
+  reticulate::import_from_path(module_name,
+                               path = module_path,
+                               convert = TRUE,
+                               delay_load = list(
+                                 before_load = gdverse_pyinit,
+                                 on_error = \(error) {
+                                   stop('python dependencies for gdverse cannot be loaded properly.')
+                                   }
+                               ))
 }
 
 #' Check Python & R sync
