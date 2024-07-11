54d2ae98b09178ebf765c3dccc7972f59d6d4da0
SpatLyu <lyu.geosocial@gmail.com>
2024-07-12 01:05:59 +0800

    rearrange source code

--- R/utils_helper.R
+++ R/utils_helper.R
@@ -1,3 +1,15 @@
+#' @title Pipe operator
+#' @description
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
+
 #' @title calculate inverse distance weight
 #' @author Wenbo Lv \email{lyu.geosocial@gmail.com}
 #' @description
@@ -125,3 +137,4 @@ weight_assign = \(x,w,list = FALSE){
     return(x * w / sum(w))
   }
 }
+
