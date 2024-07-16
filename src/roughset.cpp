2dd73c1b2adf31969aba3916e67f0ff2f3d08ac8
SpatLyu <lyu.geosocial@gmail.com>
2024-07-16 16:00:44 +0800

    add srsgd src cpp code

--- src/roughset.cpp
+++ src/roughset.cpp
@@ -1,2 +1,76 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+
+// [[Rcpp::plugins(cpp11)]]
+
+IntegerVector rcpp_which(LogicalVector x){
+  IntegerVector v = seq(0,x.size()-1);
+  return v[x];
+}
+
+bool AnyCommonElementVec2(IntegerVector vec1, IntegerVector vec2) {
+  for (int i = 0; i < vec1.size(); ++i) {
+    if (vec1[i] == vec2[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool AnyCommonElementVecMat(IntegerVector vec1, IntegerMatrix mat1) {
+  for (int i = 0; i < mat1.nrow(); ++i) {
+    IntegerVector vec2 = mat1(i,_);
+    if (AnyCommonElementVec2(vec1,vec2)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+IntegerMatrix slice_matrix_rows(IntegerMatrix mat, IntegerVector rows) {
+  int new_nrow = rows.size();
+  int ncol = mat.ncol();
+  IntegerMatrix new_mat(new_nrow, ncol);
+  for (int i = 0; i < new_nrow; ++i) {
+    int row_index = rows[i] - 1;
+    for (int j = 0; j < ncol; ++j) {
+      new_mat(i, j) = mat(row_index, j);
+    }
+  }
+  return new_mat;
+}
+
+NumericVector rcpp_log2(NumericVector vec) {
+  NumericVector res(vec.size());
+  for (int i = 0; i < vec.size(); ++i) {
+    res[i] = std::log2(vec[i]);
+  }
+  return res;
+}
+
+// [[Rcpp::export]]
+
+NumericVector SRS_PD(IntegerMatrix xobs,
+                     IntegerMatrix wt){
+  NumericVector res(xobs.nrow());
+  for (int i = 0; i < xobs.nrow(); ++i){
+    IntegerVector wti = wt(i,_);
+    wti = rcpp_which(wti != 0);
+    IntegerVector apprx(wti.size());
+    IntegerMatrix mat1 = slice_matrix_rows(xobs,wti);
+    for (int n = 0; n <= wti.size(); ++n){
+      IntegerVector vec1 = xobs(wti[n],_);
+      if (AnyCommonElementVecMat(vec1,mat1)){
+        apprx[n] = 1;
+      }
+    }
+    apprx = rcpp_which(apprx == 1);
+    res[i] = apprx.size() / wti.size();
+  }
+  double pd = Rcpp::sum(res) / xobs.nrow();
+  NumericVector pdN = res / Rcpp::sum(res);
+  double sepd = -1 * Rcpp::sum(pdN * rcpp_log2(pdN));
+  NumericVector out = NumericVector::create(Named("PD",pd),
+                                            Named("SE_PD",sepd));
+  return out;
+}
