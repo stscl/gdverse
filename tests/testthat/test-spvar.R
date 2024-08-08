de45dc6d0f07190d8ee2e3d807d75d755d0c8121
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 20:09:49 +0800

    update spvar test

--- tests/testthat/test-spvar.R
+++ tests/testthat/test-spvar.R
@@ -1,4 +1,4 @@
-test_that("spvar function works well", {
+test_that("test whether spvar function works well", {
   y = c(42,56,73)
   wt1 = inverse_distance_weight(1:length(y),1:length(y))
   expect_equal(spvar(y,wt1), 193.1)
