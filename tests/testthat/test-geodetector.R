a120dfb22583f525d61d2f1ebdbdab0d12105756
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 20:16:50 +0800

    add test for factor_detector

--- tests/testthat/test-geodetector.R
+++ tests/testthat/test-geodetector.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("test whether factor_detector function works well", {
+  fd = factor_detector(y = 1:7,x = c('x',rep('y',3),rep('z',3)))
+  fd = lapply(fd, round, 5)
+  expect_equal(fd,
+               list(`Q-statistic` = 0.77143,
+                    `P-value` = 0.07936))
 })
