34616791d0a5ac83cc630bdee77eb2fc2ff7b47b
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 20:25:21 +0800

    update round number to 6 in test

--- tests/testthat/test-geodetector.R
+++ tests/testthat/test-geodetector.R
@@ -1,7 +1,7 @@
 test_that("test whether factor_detector function works well", {
   fd = factor_detector(y = 1:7,x = c('x',rep('y',3),rep('z',3)))
-  fd = lapply(fd, round, 5)
+  fd = lapply(fd, round, 6)
   expect_equal(fd,
-               list(`Q-statistic` = 0.77143,
-                    `P-value` = 0.07936))
+               list(`Q-statistic` = 0.771429,
+                    `P-value` = 0.079365))
 })
--- tests/testthat/test-psd_spade.R
+++ tests/testthat/test-psd_spade.R
@@ -1,6 +1,6 @@
 test_that("test whether psd_spade function works well", {
   data('NTDs')
   wt = inverse_distance_weight(NTDs$X,NTDs$Y,power = 2)
-  p = round(psd_spade(NTDs$incidence,NTDs$soiltype,wt),5)
-  expect_equal(p,0.25665)
+  p = round(psd_spade(NTDs$incidence,NTDs$soiltype,wt),6)
+  expect_equal(p,0.256653)
 })
