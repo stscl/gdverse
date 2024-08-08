f0ae26cbb3addd32c68f6016c42ef64febafe130
SpatLyu <lyu.geosocial@gmail.com>
2024-08-08 20:23:40 +0800

    add test for psd_spade

---
+++ tests/testthat/test-psd_spade.R
@@ -0,0 +1,6 @@
+test_that("test whether psd_spade function works well", {
+  data('NTDs')
+  wt = inverse_distance_weight(NTDs$X,NTDs$Y,power = 2)
+  p = round(psd_spade(NTDs$incidence,NTDs$soiltype,wt),5)
+  expect_equal(p,0.25665)
+})
