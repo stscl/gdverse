e02a92654e867d36f83a3e9ae7b96f0f7efdb443
SpatLyu <lyu.geosocial@gmail.com>
2024-09-02 14:18:16 +0800

    small modify

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,7 +1,3 @@
-# Pre-compiled vignettes that need parallel computation.
-# reference: https://ropensci.org/blog/2019/12/08/precompute-vignettes/
-# Must manually move image files from `gdverse/` to `gdverse/vignettes/` after knit.
-
 devtools::load_all()
 
 knitr::knit("vignettes/GD.Rmd.orig",
