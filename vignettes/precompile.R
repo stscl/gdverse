6f7ced4f09f68c8d08c044539e4da7c549e53f2e
SpatLyu <lyu.geosocial@gmail.com>
2024-09-11 23:52:31 +0800

    update vignette

---
+++ vignettes/precompile.R
@@ -0,0 +1,22 @@
+devtools::load_all()
+
+knitr::knit("vignettes/gd.Rmd.orig",
+            "vignettes/gd.Rmd")
+
+knitr::knit("vignettes/opgd.Rmd.orig",
+            "vignettes/opgd.Rmd")
+
+knitr::knit("vignettes/sesu.Rmd.orig",
+            "vignettes/sesu.Rmd")
+
+knitr::knit("vignettes/shegd.Rmd.orig",
+            "vignettes/shegd.Rmd")
+
+knitr::knit("vignettes/spade.Rmd.orig",
+            "vignettes/spade.Rmd")
+
+knitr::knit("vignettes/idsa.Rmd.orig",
+            "vignettes/idsa.Rmd")
+
+knitr::knit("vignettes/rgdrid.Rmd.orig",
+            "vignettes/rgdrid.Rmd")
