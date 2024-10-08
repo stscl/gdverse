0c7cf7579390d6b183661dc265cb3983accc4adb
SpatLyu <lyu.geosocial@gmail.com>
2024-10-08 10:09:35 +0800

    update gd_bestunidisc

--- R/discretization.R
+++ R/discretization.R
@@ -43,17 +43,14 @@ st_unidisc = \(x,k,method = "quantile",factor = FALSE,
 #' @param formula A formula of best univariate discretization.
 #' @param data A data.frame or tibble of observation data.
 #' @param discnum (optional) A vector of number of classes for discretization. Default is `3:22`.
-#' @param discmethod (optional) A vector of methods for discretization,default is using
-#' `c("sd","equal","pretty","quantile","fisher","headtails","maximum","box")`in `gdverse`.
-#' @param cores (optional) A positive integer(default is 1). If cores > 1, a 'parallel' package
-#' cluster with that many cores is created and used. You can also supply a cluster
-#' object.
+#' @param discmethod (optional) A vector of methods for discretization, default is using
+#' `c("sd","equal","geometric","quantile","natural")`by invoking `sdsfun`.
+#' @param cores (optional) Positive integer (default is 1). When cores are greater than 1, use
+#' multi-core parallel computing.
 #' @param return_disc (optional) Whether or not return discretized result used the optimal parameter.
 #' Default is `TRUE`.
-#' @param seed (optional) Random seed number, default is `123456789`. Setting random seed is useful when
-#' the sample size is greater than `3000`(the default value for `largeN`) and the data is discretized
-#' by sampling `10%`(the default value for `samp_prop` in `st_unidisc()`).
-#' @param ... (optional) Other arguments passed to `st_unidisc()`.
+#' @param seed (optional) Random seed number, default is `123456789`.
+#' @param ... (optional) Other arguments passed to `sdsfun::discretize_vector()`.
 #'
 #' @return A list with the optimal parameter in the provided parameter combination with `k`,
 #' `method` and `disc`(when `return_disc` is `TRUE`).
@@ -67,13 +64,13 @@ st_unidisc = \(x,k,method = "quantile",factor = FALSE,
 #'
 #' @examples
 #' data('sim')
-#' gd_bestunidisc(y ~ xa + xb + xc, data = sim,
-#'                discvar = paste0('x',letters[1:3]),
+#' gd_bestunidisc(y ~ xa + xb + xc,
+#'                data = sim,
 #'                discnum = 3:6)
 #'
 gd_bestunidisc = \(formula, data, discnum = 3:22,
-                   discmethod = c("sd","equal","pretty","quantile","fisher","headtails","maximum","box"),
-                   cores = 1,return_disc = TRUE,seed = 123456789,...){
+                   discmethod = c("sd","equal","geometric","quantile","natural"),
+                   cores = 1, return_disc = TRUE, seed = 123456789, ...){
   doclust = FALSE
   if (inherits(cores, "cluster")) {
     doclust = TRUE
@@ -99,17 +96,17 @@ gd_bestunidisc = \(formula, data, discnum = 3:22,
   parak = split(paradf, seq_len(nrow(paradf)))
 
   calcul_disc = \(paramgd){
-    xdisc = st_unidisc(explanatory[,paramgd[[1]],drop = TRUE],
-                       k = paramgd[[2]],method = paramgd[[3]],
-                       seed = seed, ...)
-    fd = factor_detector(response,xdisc)
+    xdisc = sdsfun::discretize_vector(explanatory[,paramgd[[1]],drop = TRUE],
+                                      n = paramgd[[2]],method = paramgd[[3]],
+                                      seed = seed, ...)
+    fd = gdverse::factor_detector(response,xdisc)
     q = fd[[1]]
     names(q) = "qstatistic"
     return(q)
   }
 
   if (doclust) {
-    parallel::clusterExport(cores,c('st_unidisc','factor_detector'))
+    parallel::clusterExport(cores)
     out_g = parallel::parLapply(cores,parak,calcul_disc)
     out_g = tibble::as_tibble(do.call(rbind, out_g))
   } else {
@@ -126,8 +123,9 @@ gd_bestunidisc = \(formula, data, discnum = 3:22,
 
   if(return_disc){
     suppressMessages({resdisc = purrr::pmap_dfc(out_g,
-                              \(x,k,method) st_unidisc(x = explanatory[,x,drop = TRUE],
-                                                       k = k,method = method,...)) %>%
+                              \(x,k,method) sdsfun::discretize_vector(
+                                x = explanatory[,x,drop = TRUE],
+                                n = k, method = method, ...)) %>%
       purrr::set_names(out_g[[1]])})
     out_g = append(out_g,list("disv" = resdisc))
   }
