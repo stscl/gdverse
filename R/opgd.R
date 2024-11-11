fce8e065c86d3b9884e159b9dacb6c90bd192c99
SpatLyu <lyu.geosocial@gmail.com>
2024-11-11 22:08:34 +0800

    update print s3 methods

--- R/opgd.R
+++ R/opgd.R
@@ -92,7 +92,7 @@ opgd = \(formula, data, discvar = NULL, discnum = 3:8,
 #'
 print.opgd_result = \(x, ...) {
   x = x[-1] # rm opt_param to print
-  cat("***       Optimal Parameters-based Geographical Detector          \n")
+  cat("***   Optimal Parameters-based Geographical Detector     \n")
   nx = names(x)
   for (i in seq_along(x)){
     res = x[i]
