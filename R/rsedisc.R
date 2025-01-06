40a938ed7349fe43826ddb9f2aebcb7dce67fa86
SpatLyu <lyu.geosocial@gmail.com>
2025-01-06 22:16:56 +0800

    add rse_disc

---
+++ R/rsedisc.R
@@ -0,0 +1,23 @@
+rse_disc = \(formula,data,discnum,minsize = 1,cores = 1) {
+  formulavars = sdsfun::formula_varname(formula,data)
+  response = data[, formulavars[[1]], drop = TRUE]
+  explanatory = data[, formulavars[[2]]]
+  y = formula.vars[[1]]
+  xvars = formula.vars[[1]]
+  if (length(minsize)==1) {minsize = rep(1,length(xvars))}
+  if (length(discnum)==1) {discnum = rep(discnum,length(xvars))}
+  gs = as.integer(discnum)
+  minsizes = as.integer(minsize)
+  cores = as.integer(cores)
+  gdf = dplyr::select(data,dplyr::all_of(c(y,xvars)))
+  os = data %>%
+    dplyr::select(dplyr::all_of(xvars)) %>%
+    purrr::map_dfc(\(.x) sdsfun::hclustgeo_disc(.x))
+
+  script_path = system.file("python", "rse_disc.py", package = "gdverse")
+  rse_disc_lib = utils_source_python(script_path)
+  out_g = rse_disc_lib$rse_disc(gdf,y,xvars,gs,os,minsizes,cores) %>%
+    #utils_py_to_r() %>%
+    tibble::as_tibble()
+  return(out_g)
+}
