b36cf0b27de919138ed2ad60b2acb85c9afe8b9b
SpatLyu <lyu.geosocial@gmail.com>
2025-02-05 23:49:07 +0800

    update docs

--- R/geodetector.R
+++ R/geodetector.R
@@ -116,8 +116,7 @@ interaction_detector = \(y,x1,x2){
 #' @param x Covariate X, \code{factor}, \code{character} or \code{discrete numeric}.
 #' @param alpha (optional) Confidence level of the interval, default is `0.95`.
 #'
-#' @return A tibble. contains different combinations of covariate \code{X} level and student t-test statistics,
-#' degrees of freedom, p-values, and whether has risk (Yes or No).
+#' @return A tibble.
 #' @export
 #'
 #' @examples
