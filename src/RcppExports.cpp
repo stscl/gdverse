3eb40a9c6721a7dfb632ab03a1c97481f9fffe7d
SpatLyu <lyu.geosocial@gmail.com>
2026-02-08 00:00:20 +0800

    no longer initializes the RNG state

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -11,37 +11,37 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // SRS_PD
-List SRS_PD(IntegerVector yobs, IntegerMatrix xobs, IntegerMatrix wt);
+Rcpp::List SRS_PD(Rcpp::IntegerVector yobs, Rcpp::IntegerMatrix xobs, Rcpp::IntegerMatrix wt);
 RcppExport SEXP _gdverse_SRS_PD(SEXP yobsSEXP, SEXP xobsSEXP, SEXP wtSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
-    Rcpp::traits::input_parameter< IntegerVector >::type yobs(yobsSEXP);
-    Rcpp::traits::input_parameter< IntegerMatrix >::type xobs(xobsSEXP);
-    Rcpp::traits::input_parameter< IntegerMatrix >::type wt(wtSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerVector >::type yobs(yobsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerMatrix >::type xobs(xobsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerMatrix >::type wt(wtSEXP);
     rcpp_result_gen = Rcpp::wrap(SRS_PD(yobs, xobs, wt));
     return rcpp_result_gen;
 END_RCPP
 }
 // SRS_MULTIPD
-List SRS_MULTIPD(IntegerVector yobs, IntegerMatrix xobs, IntegerMatrix wt);
+Rcpp::List SRS_MULTIPD(Rcpp::IntegerVector yobs, Rcpp::IntegerMatrix xobs, Rcpp::IntegerMatrix wt);
 RcppExport SEXP _gdverse_SRS_MULTIPD(SEXP yobsSEXP, SEXP xobsSEXP, SEXP wtSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
-    Rcpp::traits::input_parameter< IntegerVector >::type yobs(yobsSEXP);
-    Rcpp::traits::input_parameter< IntegerMatrix >::type xobs(xobsSEXP);
-    Rcpp::traits::input_parameter< IntegerMatrix >::type wt(wtSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerVector >::type yobs(yobsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerMatrix >::type xobs(xobsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerMatrix >::type wt(wtSEXP);
     rcpp_result_gen = Rcpp::wrap(SRS_MULTIPD(yobs, xobs, wt));
     return rcpp_result_gen;
 END_RCPP
 }
 // SRS_PDTEST
-NumericVector SRS_PDTEST(IntegerVector yobs, IntegerMatrix xobs, IntegerMatrix wt);
+Rcpp::NumericVector SRS_PDTEST(Rcpp::IntegerVector yobs, Rcpp::IntegerMatrix xobs, Rcpp::IntegerMatrix wt);
 RcppExport SEXP _gdverse_SRS_PDTEST(SEXP yobsSEXP, SEXP xobsSEXP, SEXP wtSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
-    Rcpp::traits::input_parameter< IntegerVector >::type yobs(yobsSEXP);
-    Rcpp::traits::input_parameter< IntegerMatrix >::type xobs(xobsSEXP);
-    Rcpp::traits::input_parameter< IntegerMatrix >::type wt(wtSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerVector >::type yobs(yobsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerMatrix >::type xobs(xobsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::IntegerMatrix >::type wt(wtSEXP);
     rcpp_result_gen = Rcpp::wrap(SRS_PDTEST(yobs, xobs, wt));
     return rcpp_result_gen;
 END_RCPP
